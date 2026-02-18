3c537800e3b8089195396a35932f5fa67d3a3506
george burton <georgebburtonjr@gmail.com>
2026-02-18 17:21:24 -0500

    Delete setup.sql

--- setup.sql
+++
@@ -1,9 +0,0 @@
-use cars;
-drop table if exists car;
-create table car(
-	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    year varchar(5) not null ,
-    make varchar(20) not null ,
-    model varchar(20) not null,
-    stock INT
-    );
